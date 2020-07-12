c950a39d7c94f501a5f8b4a51a72b6e631a8d3a6
Beatriz Mayorka de Aguiar <47663726+beatrizmayorka@users.noreply.github.com>
2020-07-12 01:12:19 -0300

    Create MenuColaboradorTexto.java

---
+++ ExemploBancoPessoa/src/exemplo/ui/MenuColaboradorTexto.java
@@ -0,0 +1,96 @@
+package exemplo.ui;
+
+import exemplo.dao.ColaboradorDao;
+import exemplo.modelo.Colaborador;
+
+import java.util.List;
+
+public class MenuColaboradorTexto extends MenuEspecificoTexto{
+    private ColaboradorDao dao;
+
+    public MenuColaboradorTexto() {
+        super();
+        dao = new ColaboradorDao();
+    }
+
+    private int obterIdColaborador() {
+        System.out.print("Escolha o id do colaborador: ");
+        int id = entrada.nextInt();
+        entrada.nextLine();
+
+        return id;
+    }
+
+    private Colaborador obterDadosColaborador(Colaborador colaborador) {
+        Colaborador c;
+
+        if (fornecedor == null) {
+            c = new Colaborador(0, null, null);
+        } else {
+            c = colaborador;
+        }
+
+        System.out.print(" --> Informe o nome do colaborador: ");
+        String nome = entrada.nextLine();
+
+        System.out.print(" --> Informe o produto: ");
+        String pedidos = entrada.nextLine();
+        entrada.nextLine();
+
+        f.setNome(nome);
+
+        return f;
+    }
+
+    public void adicionar() {
+        System.out.println("Adicionar Colaborador");
+        System.out.println();
+
+        Colaborador novoColaborador = obterDadosColaborador(null);
+
+        dao.insert(novoColaborador);
+    }
+
+    public void editar() {
+        System.out.println("Editar Colaborador");
+        System.out.println();
+
+        imprimirColaborador();
+
+        int id = obterIdColaborador();
+
+        Colaborador colaboradorAModificar = dao.getById(id);
+
+        Colaborador novoColaborador = obterDadosColaborador(colaboradorAModificar);
+
+        novoColaborador.setId(colaboradorAModificar.getId());
+        dao.update(novoColaborador);
+    }
+
+    public void excluir() {
+        System.out.println("Excluir Colaborador");
+        System.out.println();
+
+        imprimirColaborador();
+        int id = obterIdColaborador();
+
+
+        dao.delete(id);
+    }
+
+    public void listarTodos() {
+        System.out.println("Lista de Colaboradores");
+        System.out.println();
+
+        imprimirColaborador();
+    }
+
+    private void imprimirColaborador() {
+        List<Colaborador> colaboradores = dao.getAll();
+        System.out.println("id\tNome\tPedidos");
+
+        for (Colaborador c : colaboradores) {
+            System.out.println(f.getId() + "\t" + f.getNome() + "\t" + f.getPedidos());
+        }
+    }
+}
