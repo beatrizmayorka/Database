6b32352a45d9cc7e78efacdc5a53e63b9ec1cfac
Beatriz Mayorka de Aguiar <47663726+beatrizmayorka@users.noreply.github.com>
2020-07-13 22:36:55 -0300

    Update MenuPrincipalTexto.java

--- ExemploBancoPessoa/src/exemplo/ui/MenuPrincipalTexto.java
+++ ExemploBancoPessoa/src/exemplo/ui/MenuPrincipalTexto.java
@@ -5,50 +5,59 @@ import java.util.Scanner;
 public class MenuPrincipalTexto {
 	
 	private static final int OP_PESSOAS = 1;
-	private static final int OP_DEPTOS = 2;
-	private static final int OP_PRODUTOS = 3;
-	private static final int OP_COLAB = 4;
-	
+	private static final int OP_DEPARTAMENTO = 2;
+	private static final int OP_PRODUTO = 3;
+	private static final int OP_COLABORADOR = 4;
+
 	private static final int OP_ADICIONAR = 1;
 	private static final int OP_LISTAR = 2;
 	private static final int OP_EDITAR = 3;
 	private static final int OP_EXCLUIR = 4;
 	
-	// conjunto de estados possiveis no sistema
-	private enum Estado {PRINCIPAL, PESSOAS, DEPTOS, PRODUTOS, COLAB};
+	private enum Estado {PRINCIPAL, PESSOAS, DEPARTAMENTO, PRODUTO, COLABORADOR};
 	
-	private Estado estadoAtual; // armazena o estado atual do menu
+	private Estado estadoAtual; 
 	private Scanner entrada;
 	
 	public MenuPrincipalTexto() {
 		estadoAtual = Estado.PRINCIPAL;
-		entrada = new Scanner(System.in); // configura o Scanner para ler da entrada padrão (STDIN)
+		entrada = new Scanner(System.in); 
 	}
 	
 	private void imprimeMenuPrincipal() {
-		System.out.println("1 - Administração de Pessoas");
-		System.out.println("2 - Administração de Departamentos");
-		System.out.println("3 - Administração de Produtos");
-		System.out.println("4 - Administração de Colaboradores");
+		System.out.println(" \t ====================================== ");
+		System.out.println(" \t | 1 - Administracao de Pessoas       | ");	
+		System.out.println(" \t | 2 - Administracao de Departamento  | ");
+		System.out.println(" \t | 3 - Administracao de Produtos      | ");
+		System.out.println(" \t | 4 - Administracao de Colaboradores | ");
+		System.out.println(" \t ====================================== ");
+
+		
 	}
 	
 	private void imprimeMenuSecundário(String tipoMenu) {
 		System.out.println("Administração de " + tipoMenu);
 		System.out.println();
-		System.out.println("1 - Adicionar");
-		System.out.println("2 - Listar");
-		System.out.println("3 - Editar");
-		System.out.println("4 - Excluir");
+		System.out.println(" \t ================= ");
+		System.out.println(" \t | 1 - Adicionar | ");
+		System.out.println(" \t ----------------- ");
+		System.out.println(" \t | 2 - Listar    | ");
+		System.out.println(" \t ----------------- ");
+		System.out.println(" \t | 3 - Editar    | ");
+		System.out.println(" \t ----------------- ");
+		System.out.println(" \t | 4 - Excluir   | ");
+		System.out.println(" \t ================= ");
 	}
 	
-	// método principal de execução do menu
 	public void executa() {
 		int opcao;
-		MenuEspecificoTexto menuEspecificoTexto;
+		MenuEspecificoTexto menuEspecificoTexto = null;
 		
 		do {
-			// Mostra o menu principal ou o menu secundário
-			System.out.println("Administração de RH"); // Título
+			System.out.println();
+			System.out.println(" \t\t ====================== ");
+			System.out.println(" \t\t |Administracao de RH | "); 
+			System.out.println(" \t\t ====================== ");
 			System.out.println();
 			
 			switch(estadoAtual) {
@@ -56,83 +65,82 @@ public class MenuPrincipalTexto {
 			case PESSOAS:
 				imprimeMenuSecundário("Pessoas");
 				break;			
-			case DEPTOS:
-				imprimeMenuSecundário("Departamentos");
+			case DEPARTAMENTO:
+				imprimeMenuSecundário("Departamento");
 				break;
-			case PRODUTOS:
+			case PRODUTO:
 				imprimeMenuSecundário("Produtos");
 				break;
-			case COLAB:
+			case COLABORADOR:
 				imprimeMenuSecundário("Colaboradores");
 				break;
-
 			default:
 				imprimeMenuPrincipal();
 			}
 			
 			System.out.println();
-			System.out.println("0 - Sair");
+			System.out.println(" \t\t ================= ");
+			System.out.println(" \t\t | 0 - Sair      | ");
+			System.out.println(" \t\t ================= ");
 			
 			System.out.println();
-			System.out.print("Escolha uma opção: ");
+			System.out.print(" --> Escolha uma opcao: ");
 	
-			// obtem entrada do usuário
 			opcao = entrada.nextInt();
 			entrada.nextLine();
 			
-			System.out.println("Voce escolheu a opção: " + opcao);
-				
+			System.out.println(" --> Voce escolheu a opcao: " + opcao);
 				
 			if (estadoAtual == Estado.PRINCIPAL) {
 				switch (opcao) {
 				case OP_PESSOAS:
 					estadoAtual = Estado.PESSOAS;
 					break;
-				case OP_DEPTOS:
-					estadoAtual = Estado.DEPTOS;
+				case OP_DEPARTAMENTO:
+					estadoAtual = Estado.DEPARTAMENTO;
 					break;
-				case OP_PRODUTOS:
-					estadoAtual = Estado.PRODUTOS;
+				case OP_PRODUTO:
+					estadoAtual = Estado.PRODUTO;
 					break;
-				case OP_COLAB:
-					estadoAtual = Estado.COLAB;
+				case OP_COLABORADOR:
+					estadoAtual = Estado.COLABORADOR;
 					break;
+				}
 				
 		
                 if (estadoAtual == Estado.PESSOAS) {
                     menuEspecificoTexto = new MenuPessoaTexto();
-                }if (estadoAtual == Estado.DEPTOS) {
-                    menuEspecificoTexto = new MenuSetorTexto(); 
-                } if (estadoAtual == Estado.PRODUTOS) {
+                }if (estadoAtual == Estado.DEPARTAMENTO) {
+                    menuEspecificoTexto = new MenuDepartamentoTexto(); 
+                } if (estadoAtual == Estado.PRODUTO) {
                     menuEspecificoTexto = new MenuProdutoTexto();
-		} if (estadoAtual == Estado.COLAB) {
-                    menuEspecificoTexto = new MenuProdutoTexto();
-          
+                } if (estadoAtual == Estado.COLABORADOR){
+                	menuEspecificoTexto = new MenuColaboradorTexto();
+                }
                  
 				switch (opcao) {
 					case OP_ADICIONAR:
-						//adicionar um item
 						menuEspecificoTexto.adicionar();
 						break;
 					case OP_EDITAR:
-						//editar um item
 						menuEspecificoTexto.editar();
 						break;
 					case OP_EXCLUIR:
-						//excluir um item
 						menuEspecificoTexto.excluir();
 						break;
 					case OP_LISTAR:
-						//listar um item
 						menuEspecificoTexto.listarTodos();
 						break;
 					default:
-						System.out.println("Opção inválida. Tente novamente!");
+						System.out.println();
+						System.out.println(" \t ==================================== ");
+						System.out.println(" \t | Opcao invalida. Tente novamente! | ");
+						System.out.println(" \t ==================================== ");
 				}
 			}
 			
 			
-		} while (opcao != 0);// enquanto o usuário não sai do sistema
+		} while (opcao != 0);
 		
 	}
 	
